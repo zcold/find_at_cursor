1f2d9c2734d9bca46d519d1b7fcb1cd670321300
Shuo Li <alwayszcold@gmail.com>
2017-10-15 20:46:58 +0200

    Create find_at_cursor.py

---
+++ find_at_cursor.py
@@ -0,0 +1,41 @@
+import sublime
+import sublime_plugin
+
+
+class FindAtCursor(sublime_plugin.TextCommand):
+
+    @property
+    def highlighted_regions(self):
+        result = []
+        for i in range(100):
+            region = self.view.get_regions('highlight_word_{}'.format(i))
+            if not region:
+                break
+            result.append(region)
+        return result
+
+    def run(self, edit, find_next=True):
+        for region in self.view.sel():
+            if region.empty():
+                region = self.view.word(region)
+            break
+
+        for word_regions in self.highlighted_regions:
+            for i, selection_region in enumerate(word_regions):
+                if selection_region.contains(region):
+                    search_word = self.view.substr(selection_region)
+                    offset = 1 if find_next else -1
+                    region = word_regions[(i + offset) % len(word_regions)]
+                    self.view.sel().clear()
+                    self.view.sel().add(region)
+                    sublime.status_message(search_word)
+                    return
+
+        self.view.sel().clear()
+        self.view.sel().add(region)
+        self.view.window().run_command('slurp_find_string')
+        self.view.window().run_command('show_panel', {'panel': 'find', 'toggle': False})
+        if find_next:
+            self.view.window().run_command('find_next')
+        else:
+            self.view.window().run_command('find_prev')
